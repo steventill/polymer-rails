d5533c0f47ed3b571f1e5253a1bc3001fa9c440c
Steven Till <till.steve@gmail.com>
2017-09-30 12:18:51 -0400

    moved component and install generators, updated files included

--- lib/html-importer-rails.rb
+++ lib/html-importer-rails.rb
@@ -4,4 +4,6 @@ require "html-importer/xml_adapters/base"
 require "html-importer/processors/directive"
 require "html-importer/processors/component"
 require "html-importer/helpers/asset_tag_helper"
+require 'generators/html-importer/component/component_generator'
+require 'generators/html-importer/install/install_generator'
 require "html-importer/railtie" if defined?(::Rails)
