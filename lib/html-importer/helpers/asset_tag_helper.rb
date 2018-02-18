e8617ca6f6f703449d26f599d43415147dd944be
Steven Till <till.steve@gmail.com>
2018-02-18 10:24:37 -0500

    updated generator for the style to be in the template

--- lib/html-importer/helpers/asset_tag_helper.rb
+++ lib/html-importer/helpers/asset_tag_helper.rb
@@ -4,10 +4,19 @@ module HtmlImporter
       def html_import_tag(*sources)
         options = sources.extract_options!
         path_options = options.extract!('protocol').symbolize_keys
+        
         sources.uniq.map do |source|
-          tag_options = { rel: "import", href: path_to_asset(source, {type: :html, extname: '.html'}.merge!(path_options)) }
-          tag(:link, tag_options.merge!(options).stringify_keys)
+          _html_import_tag source, options, path_options
         end.join("\n").html_safe
+      end
+      
+      def _html_import_tag source, options, path_options
+        tag_options = { 
+          rel: "import", 
+          href: path_to_asset(source, {type: :html, extname: '.html'}.merge!(path_options))
+        }
+  
+        tag(:link, tag_options.merge!(options).stringify_keys)
       end
     end
   end
